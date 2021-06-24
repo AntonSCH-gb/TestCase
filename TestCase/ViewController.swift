cb2e87f109a97fe984418baba4938568a3e75e49
AntonSCH <antonsch@mail.ru>
2021-06-24 11:28:02 +0300

    initial commit

---
+++ TestCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestCase
+//
+//  Created by Anton Scherbaev on 24.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
